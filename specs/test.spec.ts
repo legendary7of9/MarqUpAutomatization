861132f8f37e512ff467c89f46fe7e705ba9abbf
legendary7of9 <banshchykov@gmail.com>
2022-02-12 00:35:40 +0200

    test commit

--- specs/test.spec.ts
+++ specs/test.spec.ts
@@ -26,28 +26,4 @@ test.beforeEach(async ({ page }) => {
     await sideBarMenu.sideBarModelClick();
     await model.addModelButtonClick();
     await model.addModelChooseButtonClick();
-}); 
-
-test('choosingExistingModel @regChecklistNewHigh @existingModelPage', async ({ page }) => {
-    const existingModel = new ExistingModelPage(page);
-    const checkedUnchecked0 = page.locator('mat-radio-button >> nth=3');
-    const checkedUnchecked1 = page.locator('mat-radio-button >> nth=6');
-    const checkedUnchecked2 = page.locator('mat-radio-button >> nth=8');
-    console.log('existingModelPage Choosing Existing Model');
-    await existingModel.searchModelsUse();
-    await expect(checkedUnchecked0).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await expect(checkedUnchecked1).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await expect(checkedUnchecked2).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await existingModel.radioButtonChooseZero();
-    await expect(checkedUnchecked0).toHaveClass('select-model-item mat-radio-button mat-primary mat-radio-checked');
-    await expect(checkedUnchecked1).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await expect(checkedUnchecked2).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await existingModel.radioButtonChooseOne();
-    await expect(checkedUnchecked0).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await expect(checkedUnchecked1).toHaveClass('select-model-item mat-radio-button mat-primary mat-radio-checked');
-    await expect(checkedUnchecked2).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await existingModel.radioButtonChooseTwo();
-    await expect(checkedUnchecked0).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await expect(checkedUnchecked1).toHaveClass('select-model-item mat-radio-button mat-primary');
-    await expect(checkedUnchecked2).toHaveClass('select-model-item mat-radio-button mat-primary mat-radio-checked');   
-    })
+});
